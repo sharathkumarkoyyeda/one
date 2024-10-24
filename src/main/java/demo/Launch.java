109fb0314ced2e9934a62f020edd025ae7a34b14
ShatahKoyyeda <ShatahkumarKoyyeda@gmail.com>
2024-10-24 12:47:51 +0530

    fc

---
+++ src/main/java/demo/Launch.java
@@ -0,0 +1,18 @@
+package demo;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launch {
+
+        public  static void main(String[] args)
+        {
+            WebDriverManager.chromedriver().setup();
+            ChromeDriver d = new ChromeDriver();
+            d.manage().window().maximize();
+            d.get("https://www.youtube.com/user/Youtube");
+            System.out.println(" LauncH");
+            d.close();
+        }
+
+}
